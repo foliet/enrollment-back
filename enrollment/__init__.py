8373df5d2ef37704cd2c7319a6a8a8f5fd5595e8
foliet <foliet@rainspace.cn>
2022-06-13 14:22:40 +0800

    feat:使用cluster集群连接池

--- enrollment/__init__.py
+++ enrollment/__init__.py
@@ -1,7 +1,11 @@
 import pymysql
-import redis
-
+from rediscluster import ClusterConnectionPool, RedisCluster
 pymysql.install_as_MySQLdb()
 
-pool = redis.ConnectionPool(host='10.0.16.17', port=6379, password='fqh66545896.')
-redis_conn = redis.Redis(connection_pool=pool)
+nodes = [
+    {'host': '10.0.16.17', 'port': 6379},
+    {'host': '10.0.4.2', 'port': 6379},
+    {'host': '10.0.4.4', 'port': 6379}
+]
+pool = ClusterConnectionPool(startup_nodes=nodes)
+redis_conn = RedisCluster(connection_pool=pool)
